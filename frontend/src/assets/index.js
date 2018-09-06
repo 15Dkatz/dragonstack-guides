9987519ee1bbb913a4b1a41aaf412aee923323f6
David Katz <15Dkatz@shcp.edu>
2018-09-05 19:32:53 -0700

    Add assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,10 @@
+import skinny from './skinny.png';
+import slender from './slender.png';
+import sporty from './sporty.png';
+import stocky from './stocky.png';
+import patchy from './patchy.png';
+import plain from './plain.png';
+import spotted from './spotted.png';
+import striped from './striped.png';
+
+export { skinny, slender, sporty, stocky, patchy, plain, spotted, striped };
