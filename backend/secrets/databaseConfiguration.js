e9d6fdbca1a58170710f52eee9261c33eb470f29
David Katz <15Dkatz@shcp.edu>
2018-09-27 09:14:36 -0700

    Include backend secrets as part of example

---
+++ backend/secrets/databaseConfiguration.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: 'node_user',
+  host: 'localhost',
+  database: 'dragonstackdb',
+  password: 'node_password',
+  port: 5432
+};
---
+++ backend/secrets/index.js
@@ -0,0 +1,3 @@
+const APP_SECRET = 'foo123!123foo';
+
+module.exports = { APP_SECRET }
