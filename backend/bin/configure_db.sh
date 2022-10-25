550f0d228c9cc161393048e60f408b739798aaaf
A. Peixinho <a_peixinho@sapo.pt>
2022-10-25 19:39:49 +0200

    Updated gitignore Added docker compose for DB

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-export PGPASSWORD='node_password'
+# export PGPASSWORD='node_password'
 
 echo "Configuring dragonstackdb"
 
-dropdb -U node_user dragonstackdb
-createdb -U node_user dragonstackdb
+# dropdb -U node_user dragonstackdb
+# createdb -U node_user dragonstackdb
 
-psql -U node_user dragonstackdb < ./bin/sql/account.sql
-psql -U node_user dragonstackdb < ./bin/sql/generation.sql
-psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
-psql -U node_user dragonstackdb < ./bin/sql/trait.sql
-psql -U node_user dragonstackdb < ./bin/sql/dragonTrait.sql
-psql -U node_user dragonstackdb < ./bin/sql/accountDragon.sql
+# psql -U node_user dragonstackdb < ./bin/sql/account.sql
+# psql -U node_user dragonstackdb < ./bin/sql/generation.sql
+# psql -U node_user dragonstackdb < ./bin/sql/dragon.sql
+# psql -U node_user dragonstackdb < ./bin/sql/trait.sql
+# psql -U node_user dragonstackdb < ./bin/sql/dragonTrait.sql
+# psql -U node_user dragonstackdb < ./bin/sql/accountDragon.sql
 
 node ./bin/insertTraits.js
 
